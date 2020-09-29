f770e35222ec9051f3e0ac3eba916144bbff904b
abdulsalam12000@yahoo.com <abdulsalam12000@yahoo.com>
2020-09-29 23:58:50 +0100

    more changes to testrunner

--- src/test/java/step_defintions/Testrunner.java
+++ src/test/java/step_defintions/Testrunner.java
@@ -5,6 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/resources/feature_files", plugin = {"pretty", "html:target/cucumber.html",  "json:target/cucumber/cucumber-report.json", "junit:target/cucumber.xml"}, tags="@RunallTest")
+@CucumberOptions(features = "src/main/resources/feature_files",
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber/cucumber-report.json",
+                "junit:target/cucumber.xml"
+        }, tags="@RunallTest")
 public class Testrunner {
 }
